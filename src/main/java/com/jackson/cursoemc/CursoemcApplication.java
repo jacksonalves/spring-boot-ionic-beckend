4e68bc00e1a917bfaceea15958e84b41ff15ad17
jacksonalves <jacksonalves@gmail.com>
2018-03-17 11:30:36 -0300

    projeto criado

---
+++ src/main/java/com/jackson/cursoemc/CursoemcApplication.java
@@ -0,0 +1,12 @@
+package com.jackson.cursoemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoemcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoemcApplication.class, args);
+	}
+}
