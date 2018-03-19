26a1719cad38feab0dfcc0850150abe69f863d2b
jacksonalves <jacksonalves@gmail.com>
2018-03-19 18:44:09 -0300

    Endpoint /cliente/{id} disponivel

---
+++ src/main/java/com/jackson/cursoemc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.jackson.cursoemc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jackson.cursoemc.domain.Cliente;
+import com.jackson.cursoemc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id)  {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new com.jackson.cursoemc.services.exception.ObjectNotFoundException(
+					"Objeto não encontrado! id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
