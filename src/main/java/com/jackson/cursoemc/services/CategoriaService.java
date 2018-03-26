1ffd8c903f10101053945bf486d8dd78094900f8
jacksonalves <jacksonalves@gmail.com>
2018-03-26 14:06:12 -0300

    Inserindo nova Categoria com POST

--- src/main/java/com/jackson/cursoemc/services/CategoriaService.java
+++ src/main/java/com/jackson/cursoemc/services/CategoriaService.java
@@ -20,4 +20,9 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new com.jackson.cursoemc.services.exception.ObjectNotFoundException(
 					"Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
