aec00bf86d32e05687215347b26829b792d9c785
jacksonalves <jacksonalves@gmail.com>
2018-04-03 08:50:46 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/jackson/cursoemc/resouces/exception/FieldMessege.java
@@ -0,0 +1,39 @@
+package com.jackson.cursoemc.resouces.exception;
+
+import java.io.Serializable;
+
+public class FieldMessege implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String fieldName;
+	private String message;
+	
+	public FieldMessege() {
+		
+	}
+
+	public FieldMessege(String fieldName, String message) {
+		super();
+		this.fieldName = fieldName;
+		this.message = message;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+
+}
