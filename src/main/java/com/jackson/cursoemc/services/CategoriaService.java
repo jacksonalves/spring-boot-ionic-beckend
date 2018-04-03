c4e0475550c213be6cb523738c1c316422de4402
jacksonalves <jacksonalves@gmail.com>
2018-04-03 10:56:27 -0300

    Correções em CategoriaService costrutor Pagamentp

--- src/main/java/com/jackson/cursoemc/services/CategoriaService.java
+++ src/main/java/com/jackson/cursoemc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jackson.cursoemc.domain.Categoria;
+import com.jackson.cursoemc.domain.Cliente;
 import com.jackson.cursoemc.dto.CategoriaDTO;
 import com.jackson.cursoemc.repositories.CategoriaRepository;
 import com.jackson.cursoemc.services.exception.DataIntegrityException;
@@ -34,8 +35,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -59,6 +61,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(),objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 	
 }
