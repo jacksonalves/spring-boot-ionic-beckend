ce447f05c90fa89e8cbce57a38a73d2a4dd8980a
jacksonalves <jacksonalves@gmail.com>
2018-04-04 23:04:14 -0300

    validação de email não repetido na inserção de Cliente

--- src/main/java/com/jackson/cursoemc/services/validation/ClienteInsertValidation.java
+++ src/main/java/com/jackson/cursoemc/services/validation/ClienteInsertValidation.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jackson.cursoemc.domain.Cliente;
 import com.jackson.cursoemc.domain.enums.TipoCliente;
 import com.jackson.cursoemc.dto.ClenteNewDTO;
+import com.jackson.cursoemc.repositories.ClienteRepository;
 import com.jackson.cursoemc.resouces.exception.FieldMessege;
 import com.jackson.cursoemc.services.validation.utils.BR;
 
 public class ClienteInsertValidation implements ConstraintValidator<ClienteInsert, ClenteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 		
@@ -30,6 +37,10 @@ public class ClienteInsertValidation implements ConstraintValidator<ClienteInser
 			list.add(new FieldMessege("CpfOuCnpj", "CNPJ inválido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessege("email", "Email já existente"));
+		}
 		
 		for (FieldMessege e : list) {
 			context.disableDefaultConstraintViolation();
