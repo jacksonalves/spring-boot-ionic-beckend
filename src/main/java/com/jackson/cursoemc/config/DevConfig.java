ed77e1d7f4039054ec56c927a43b7eda39cc55bd
jacksonalves <jacksonalves@gmail.com>
2018-04-16 08:46:04 -0300

    criando profile de desenvolvimento

---
+++ src/main/java/com/jackson/cursoemc/config/DevConfig.java
@@ -0,0 +1,26 @@
+package com.jackson.cursoemc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.jackson.cursoemc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+		
+		dbService.instatiateTestDataBase();
+		
+		return true;
+	}
+}
