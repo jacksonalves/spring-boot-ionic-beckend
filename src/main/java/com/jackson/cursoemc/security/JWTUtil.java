1cd9bcc82b152b497b2975a9cb7fd2ca18b63276
jacksonalves <jacksonalves@gmail.com>
2018-05-01 22:55:14 -0300

    Implementando autorização

--- src/main/java/com/jackson/cursoemc/security/JWTUtil.java
+++ src/main/java/com/jackson/cursoemc/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -23,4 +24,36 @@ public class JWTUtil {
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
 	}
+	
+	public boolean tokenValido(String token) {
+		Claims clains = getClaims(token);
+		if(clains != null) {
+			String usernmae = clains.getSubject();
+			Date expirationDate = clains.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			if(usernmae != null && expirationDate != null && now.before(expirationDate));
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public String getUserName(String token) {
+		Claims clains = getClaims(token);
+		if(clains != null) {
+			return clains.getSubject();
+		}
+		return null;
+	}
+	
+	private Claims getClaims(String token) {
+		try {
+		return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+		
+	
 }
