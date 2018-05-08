7783f2dbd05d24d9970a23e315e0d98cc4706566
jacksonalves <jacksonalves@gmail.com>
2018-05-08 08:14:58 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/jackson/cursoemc/repositories/PedidoRepository.java
+++ src/main/java/com/jackson/cursoemc/repositories/PedidoRepository.java
@@ -1,12 +1,18 @@
 package com.jackson.cursoemc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.jackson.cursoemc.domain.Cliente;
 import com.jackson.cursoemc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
-
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 	
 }
--- src/main/java/com/jackson/cursoemc/services/PedidoService.java
+++ src/main/java/com/jackson/cursoemc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jackson.cursoemc.domain.Cliente;
 import com.jackson.cursoemc.domain.ItemPedido;
 import com.jackson.cursoemc.domain.PagamentoComBoleto;
 import com.jackson.cursoemc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.jackson.cursoemc.domain.enums.EstadoPagamento;
 import com.jackson.cursoemc.repositories.ItemPedidoRepository;
 import com.jackson.cursoemc.repositories.PagamentoRepository;
 import com.jackson.cursoemc.repositories.PedidoRepository;
+import com.jackson.cursoemc.security.UserSS;
+import com.jackson.cursoemc.services.exception.AuthorizationException;
 
 @Service
 public class PedidoService {
@@ -38,6 +44,7 @@ public class PedidoService {
 	
 	@Autowired
 	private EmailService emailService;
+	
 	
 	@SuppressWarnings("unused")
 	@Autowired
@@ -71,6 +78,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.autenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 }
